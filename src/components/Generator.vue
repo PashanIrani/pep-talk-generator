dfaca97429126e7518f6b3648c729394831ceb0e
PashanIrani <pashanirani@gmail.com>
2021-10-18 10:21:28 -0700

    added memory to randomizer

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -11,19 +11,36 @@ import data from "../config/pep-talk-data";
 export default {
   name: "Generator",
   data: () => {
-    return { text: String };
+    return { text: String, seen: [[], [], [], []] };
   },
   created() {
     this.generate();
   },
   methods: {
-    generate() {
+    getRandom() {
       let picks = [
-        Math.floor(Math.random() * data["1"].length),
-        Math.floor(Math.random() * data["2"].length),
-        Math.floor(Math.random() * data["3"].length),
-        Math.floor(Math.random() * data["4"].length),
+        Math.floor(Math.random() * data[1].length),
+        Math.floor(Math.random() * data[2].length),
+        Math.floor(Math.random() * data[3].length),
+        Math.floor(Math.random() * data[4].length),
       ];
+
+      for (let i = 0; i < picks.length; i++) {
+        picks[i] = Math.floor(Math.random() * data[i + 1].length);
+
+        if (this.seen[i].length >= 8) this.seen[i] = [];
+
+        while (this.seen[i].includes(picks[i])) {
+          picks[i] = Math.floor(Math.random() * data[i + 1].length);
+        }
+
+        this.seen[i].push(picks[i]);
+      }
+
+      return picks;
+    },
+    generate() {
+      let picks = this.getRandom();
 
       this.text =
         data["1"][picks[0]] +
